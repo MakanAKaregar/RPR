b84f4b469ba884ee5d25320696a52e5bef186241
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2022-07-05 15:41:18 +0200

    Update sync_nextCloudData.py

--- pyCodes/sync_nextCloudData.py
+++ pyCodes/sync_nextCloudData.py
@@ -1,8 +1,8 @@
 #add this code to crontab 
 #*/1 * * * * /bin/python3.9 /home/pi/RPR/pyCodes/sync_nextCloudData.py
 
-nextcloud_localDir = '/home/pi/RPR/data'#local dir for nextcloud
-streaming_interval = 60 #streaming interval in second. Best to be equal to interval to be set in crontab 
+nextcloud_localDir = '/home/pi/RPR/data'#RPR’s NMEA data driectory
+streaming_interval = 60 #streaming interval in second. Best to be equal to the interval given in your cron job 
 
 from os import *
 import time
