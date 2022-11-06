1c2c1c719a671ff66f10fbb80ded5cda7b8992c2
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2022-11-06 11:16:42 +0100

    Update powerOffLan.py

--- pyCodes/powerOffLan.py
+++ pyCodes/powerOffLan.py
@@ -4,7 +4,7 @@
 #@reboot sudo /bin/python3.7 /home/pi/RPR/pyCodes/powerOffLan.py
 
 import os
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 1 -p 0")
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 1 -p 0")
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 1 -p 0")
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 1 -p 0")
 
 
