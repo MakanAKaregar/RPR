33d81aeb6e81e8553f55cc29cf00ce6cd98d4e75
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2022-11-06 12:21:15 +0100

    Update powerOffUSBdongle.py

--- pyCodes/powerOffUSBdongle.py
+++ pyCodes/powerOffUSBdongle.py
@@ -11,23 +11,23 @@ import os
 import time
 os.system("sudo pkill -f dataPicker.py")#kill dataPicker.py
 time.sleep(1)
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 1 -P 2 -p 0")#power off all usb
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 1 -P 2 -p 0")#power off all usb
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 1 -P 2 -p 0")#power off all usb
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 1 -P 2 -p 0")#power off all usb
 time.sleep(2)
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 1 -P 2 -p 1")#power on all usb
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 1 -P 2 -p 1")#power on all usb
 time.sleep(2)
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 1 -P 3 -p 0")#power off usb D (dongle)
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 1 -P 3 -p 0")#power off usb D (dongle)
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 1 -P 3 -p 0")#power off usb D (dongle)
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 1 -P 3 -p 0")#power off usb D (dongle)
 
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 2 -p 1")#power on usb A
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 2 -p 1")#power on usb A
 time.sleep(2)
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 2 -p 0")#power off usb A 
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 2 -p 0")#power off usb A
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 2 -p 0")#power off usb A 
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 2 -p 0")#power off usb A
 
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 3 -p 1")#power on usb B
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 3 -p 1")#power on usb B
 time.sleep(2)
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 3 -p 0")#power off usb B 
-os.system("cd /home/pi/RPR/hub-ctrl.c-master ; sudo ./hub-ctrl -h 0 -P 3 -p 0")#power off usb B
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 3 -p 0")#power off usb B 
+os.system("cd /home/pi/RPR/hub-ctrl.c ; sudo ./hub-ctrl -h 0 -P 3 -p 0")#power off usb B
 
 time.sleep(3)
 os.system('/bin/python3.7 /home/pi/RPR/pyCodes/dataPicker.py')#run dataPicker.py
