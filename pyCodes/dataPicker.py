a27526586a031d7a5db834d6eea50029c6bd535c
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2022-12-16 11:14:00 +0100

    Update dataPicker.py

--- pyCodes/dataPicker.py
+++ pyCodes/dataPicker.py
@@ -17,18 +17,22 @@ baud_rate = 9600; # in your IDE code, that is Serial.begin(baud_rate)
 
 ser_ls = serial.Serial(serial_port, baud_rate)#listen to serial port 
 
+#initialization date
+RunD = datetime.datetime.now().day
+RunM = datetime.datetime.now().month
+RunY = datetime.datetime.now().year
+
 while True:
     ln = ser_ls.readline();#read a line from serial port
-    
     ln = ln.decode("utf-8") #convert binary to string
-
-    if len(ln)==12 and ln[0:10]!=filename:#check if the first line is "filename.log" and differs to what you defined previously (line 11)
-        filename=ln[0:10];#replace filename if differs from the one defined  
+    print(ln);
+    
+    #check the current date with initialization date (as in RunD, RunM and RunY)
+    if (datetime.datetime.now().year == RunY and datetime.datetime.now().month == RunM and datetime.datetime.now().day == RunD):
+        output_file.write(ln);
+    else:
+        date_now=str(datetime.datetime.now());# current date and time
+        filename=(date_now[2:4]+date_now[5:7]+date_now[8:10]+'.log');# filename is set at the daily basis
         output_file = open(file_path+filename,"a");
         print(ln);
         output_file.write(ln);
-    else:
-    
-        print(ln);
-        output_file.write(ln);
-     
