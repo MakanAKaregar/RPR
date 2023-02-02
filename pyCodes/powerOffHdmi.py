a5f51aadd7c7a4965bab4228f9decf1d6ff50c5d
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2023-02-02 17:40:07 +0100

    Update powerOffHdmi.py
    updated tvservice path for Bullseye release

--- pyCodes/powerOffHdmi.py
+++ pyCodes/powerOffHdmi.py
@@ -4,5 +4,6 @@
 #@reboot sleep 30 && sudo /bin/python3.7 /home/pi/RPR/pyCodes/powerOffHdmi.py
 
 import os
-os.system('sudo /opt/vc/bin/tvservice -o')
+#os.system('sudo /opt/vc/bin/tvservice -o')
+os.system('sudo /usr/bin/tvservice -o')
 
