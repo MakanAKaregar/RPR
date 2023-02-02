1d9bcf8d64b7dfaf0b22a93d050574733a3cd5f3
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2023-02-02 17:40:25 +0100

    Update powerOffHdmi.py

--- pyCodes/powerOffHdmi.py
+++ pyCodes/powerOffHdmi.py
@@ -1,7 +1,7 @@
 #to power off HDMI ports
 #useage example:
 #to be added to the crontab to switch off the HDMI ports after 30 sec from boot
-#@reboot sleep 30 && sudo /bin/python3.7 /home/pi/RPR/pyCodes/powerOffHdmi.py
+#@reboot sleep 30 && sudo /bin/python3.9 /home/pi/RPR/pyCodes/powerOffHdmi.py
 
 import os
 #os.system('sudo /opt/vc/bin/tvservice -o')
