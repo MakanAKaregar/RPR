49458b0dd10a1906edb1cbe97962b82ae8dbf7f0
Makan Karegar <53965061+MakanAKaregar@users.noreply.github.com>
2023-04-29 07:20:03 +0200

    Update dataPicker.py

--- pyCodes/dataPicker.py
+++ pyCodes/dataPicker.py
@@ -6,9 +6,9 @@ import serial# don't forget to install the serial package
 from datetime import timezone
 import datetime
 
-file_path="/home/pi/RPR/data/" # path for saving daily GPS SNR data
+file_path="/home/pi/RPR/data/"
 
-date_now=str(datetime.datetime.now());# current date and time
+date_now=str(datetime.datetime.now(timezone.utc)); #current date and UTC time
 
 filename=(date_now[2:4]+date_now[5:7]+date_now[8:10]+'.log');# filename is set at the daily basis
 output_file = open(file_path+filename,"a");#open output file
@@ -18,21 +18,28 @@ baud_rate = 9600; # in your IDE code, that is Serial.begin(baud_rate)
 
 ser_ls = serial.Serial(serial_port, baud_rate)#listen to serial port 
 
-#getting current UTC date
-RunD = datetime.datetime.now(timezone.utc).day
-RunM = datetime.datetime.now(timezone.utc).month
-RunY = datetime.datetime.now(timezone.utc).year
+#getting the initialization date (UTC time)
+date_ini = datetime.datetime.now(timezone.utc) 
+RunDy = date_ini.day
+RunMo = date_ini.month
+RunYr = date_ini.year
 
 while True:
     ln = ser_ls.readline();#read a line from serial port
     ln = ln.decode("utf-8") #convert binary to string
     print(ln);
     
-    #check the current date with initialization date (as in RunD, RunM and RunY)
-    if (datetime.datetime.now(timezone.utc).year == RunY and datetime.datetime.now(timezone.utc).month == RunM and datetime.datetime.now(timezone.utc).day == RunD):
+    #getting the current date (UTC time)
+    date_now = datetime.datetime.now(timezone.utc) + datetime.timedelta(seconds=18) 
+    NowDy = date_now.day
+    NowMo = date_now.month
+    NowYr = date_now.year
+
+    #check the current date with initialization date (as in RunDy, RunMo and RunYr)
+    if (NowYr == RunYr and NowMo == RunMo and NowDy == RunDy):
         output_file.write(ln);
     else:
-        date_now=str(datetime.datetime.now(timezone.utc));# current date 
+        date_now=str(datetime.datetime.now(timezone.utc));# current UTC date 
         filename=(date_now[2:4]+date_now[5:7]+date_now[8:10]+'.log');# filename is set at the daily basis
         output_file = open(file_path+filename,"a");
         print(ln);
